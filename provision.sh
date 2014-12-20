a8e92d6bcaa8c589990210fca3b2a04fd31a91cc
Louis Dorard <louis@dorard.me>
2014-12-20 16:38:15 +0100

    Add provisioning instructions from conda-base

--- provision.sh
+++ provision.sh
@@ -11,44 +11,27 @@ conda install pip --yes # or, with apt-get: apt-get install -y python-pip
 ####################
 
 pip install bigml
+pip install bigmler
 
 
-#########################
-# Google Prediction API #
-#########################
+# #########################
+# # Google Prediction API #
+# #########################
 
-# install Google API Python client
-pip install --upgrade google-api-python-client
+# # install Google API Python client
+# pip install --upgrade google-api-python-client
 
-# install gsutil to upload stuff to Google Cloud Storage
-# unfortunately, `pip install -U gsutil` is unstable, so we install it "old-school" (see https://developers.google.com/storage/docs/gsutil_install)
-wget http://storage.googleapis.com/pub/gsutil.tar.gz
-sudo mv gsutil.tar.gz /usr/lib
-sudo tar xfz /usr/lib/gsutil.tar.gz -C /usr/lib/
-echo "export PATH=\"\$PATH:/usr/lib/gsutil\"" >> ~/.bashrc
-echo "export PYTHONPATH=${PYTHONPATH}:/usr/lib/gsutil/third_party/boto:/usr/lib/gsutil" >> ~/.bashrc
-source ~/.bashrc
+# # install gsutil to upload stuff to Google Cloud Storage
+# # unfortunately, `pip install -U gsutil` is unstable, so we install it "old-school" (see https://developers.google.com/storage/docs/gsutil_install)
+# wget http://storage.googleapis.com/pub/gsutil.tar.gz
+# sudo mv gsutil.tar.gz /usr/lib
+# sudo tar xfz /usr/lib/gsutil.tar.gz -C /usr/lib/
+# echo "export PATH=\"\$PATH:/usr/lib/gsutil\"" >> ~/.zshrc
+# echo "export PYTHONPATH=${PYTHONPATH}:/usr/lib/gsutil/third_party/boto:/usr/lib/gsutil" >> ~/.zshrc
+# source ~/.zshrc
 
-# apt-get install python-socksipy
-# pip install retry_decorator
-# @todo: what's that? where does it say we need it?
-
-
-#####################
-# Optional installs #
-#####################
-
-sudo apt-get update
-
-# in case we need to git-clone repos (but versioning would be done from the host, not the VM)
-sudo apt-get install -y git-core
-
-# optional: install vim
-sudo apt-get install -y vim
-
-# optional: use zsh and oh-my-zsh
-sudo apt-get install -y zsh
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-sudo chsh -s /usr/bin/zsh vagrant # change shell for 'vagrant' user
+# # apt-get install python-socksipy
+# # pip install retry_decorator
+# # @todo: what's that? where does it say we need it?
 
 echo "... and we're done!"
