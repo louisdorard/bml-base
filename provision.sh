6c6e3fb32e7e5c5173ba264f1fa347c5b3c262ed
Louis Dorard <louis@dorard.me>
2014-12-20 16:39:37 +0100

    Add provisioning instructions from conda-base

--- provision.sh
+++ provision.sh
@@ -1,8 +1,44 @@
-# This provisioning script should be run by the 'vagrant' user
+# When using vagrant, this provisioning script should be run by the 'vagrant' user
 
 echo "Provisioning the Bootstrapping Machine Learning base box (bml-base)..."
 
-# install the pip installer which is used to install the rest
+# Base stuff
+####
+
+# Make sure we're up to date
+echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list
+sudo apt-get update && apt-get upgrade -y
+
+# optional package installations
+# * vim for text editing from the shell
+# * git for when we need to git clone repos (but versioning would be done from the host, not the VM)
+# * zsh (to use oh-my-zsh)
+# * s3cmd
+# * curl (to perform API requests and to download stuff)
+sudo apt-get install -y wget git vim zsh s3cmd curl
+
+
+# Conda stuff
+####
+
+# Install "mini" anaconda python distribution (python 2.7) and a couple of packages
+wget http://repo.continuum.io/miniconda/Miniconda-3.5.2-Linux-x86_64.sh
+mkdir /work
+/bin/bash Miniconda-3.5.2-Linux-x86_64.sh -b -p /work/anaconda/
+rm Miniconda-3.5.2-Linux-x86_64.sh
+/work/anaconda/bin/conda update conda --yes
+/work/anaconda/bin/conda install pandas scikit-learn --yes
+echo "export PATH=\"\$PATH:/work/anaconda/bin\"" >> ~/.zshrc
+echo "export PATH=\"\$PATH:/work/anaconda/bin\"" >> ~/.bashrc
+source ~/.bashrc
+
+# IPython notebooks
+conda install ipython-notebook --yes
+mkdir -p /notebooks
+echo "alias ipynb='ipython notebook --ip=0.0.0.0 /notebooks/'" >> ~/.zshrc
+echo "alias ipynb='ipython notebook --ip=0.0.0.0 /notebooks/'" >> ~/.bashrc
+
+# Install the pip installer which is often used to install additional packages
 conda install pip --yes # or, with apt-get: apt-get install -y python-pip 
 
 
